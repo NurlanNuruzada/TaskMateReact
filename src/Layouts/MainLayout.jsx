968fe7828e3db367cf9a90d4af1f63115cb043e4
nurlannuruzada <nurlangn@code.edu.az>
2024-03-08 15:49:31 +0400

    Template Done

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from '../Components/Header/Header'
 import Styles from "../Layouts/MainLayout.module.css"
-import { ChakraProvider } from '@chakra-ui/react'
 import Footer from '../Components/Footer/Footer'
 export function MainLayout() {
   return (
     <div className={Styles.Main}>
       <div className={Styles.Outlet}>
-        <ChakraProvider >
-          <Header ShowButtons={true} />
-        </ChakraProvider>
+        <Header ShowButtons={true} />
         <Outlet />
         <Footer />
       </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import Styles from './Home.module.scss'
-import Button from '../../Components/button/Button'
+import Alert from 'react-bootstrap/Alert';
 
 export default function Home() {
   return (
     <>
-      <div className={Styles.MainContainer}>
+      <div  className={Styles.MainContainer}>
         home
-        <Button />
+        <div style={{color:"red"}} className="fs-2 pb-3">huasdfsd</div>
+        <div className="ads2">huasdfsd</div>
+        <div className="ads 123">huasdfsd</div>
+        <button className={Styles.button32}></button>
+        <>
+          {[
+            'primary',
+            'secondary',
+            'success',
+            'danger',
+            'warning',
+            'info',
+            'light',
+            'dark',
+          ].map((variant) => (
+            <Alert key={variant} variant={variant}>
+              This is a {variant} alert—check it out!
+            </Alert>
+          ))}
+        </>
       </div>
     </>
   )
