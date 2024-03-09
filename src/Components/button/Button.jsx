a9a506e1c6d9c15915dcc9ccabd4b0c9dfa340c4
rftglyv <rftglyv@gmail.com>
2024-03-09 06:12:50 +0400

    Working on header #1

--- src/Components/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Styles from './Button.module.css'
-export default function Button() {
-  return (
-    <div className={Styles.Main}>Button</div>
-  )
-}
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import Styles from './Home.module.scss'
-import Alert from 'react-bootstrap/Alert';
 
 export default function Home() {
   return (
     <>
-      <div  className={Styles.MainContainer}>
-        home
-        <div style={{color:"red"}} className="fs-2 pb-3">huasdfsd</div>
-        <div className="ads2">huasdfsd</div>
-        <div className="ads 123">huasdfsd</div>
-        <button className={Styles.button32}></button>
-        <>
-          {[
-            'primary',
-            'secondary',
-            'success',
-            'danger',
-            'warning',
-            'info',
-            'light',
-            'dark',
-          ].map((variant) => (
-            <Alert key={variant} variant={variant}>
-              This is a {variant} alert—check it out!
-            </Alert>
-          ))}
-        </>
+      <div className={Styles.MainContainer}>
       </div>
     </>
   )
